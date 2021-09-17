7b001f4106359c78863b09c0b503f04d28a9d37a
leruetkins <leruetkins@gmail.com>
2021-09-17 11:55:40 +0300

    Update ESP8266ZabbixSender.cpp

--- ESP8266ZabbixSender.cpp
+++ ESP8266ZabbixSender.cpp
@@ -1,14 +1,14 @@
-#include "ESP8266ZabbixSender.h"
+#include "ESP32ZabbixSender.h"
 
-ESP8266ZabbixSender::ESP8266ZabbixSender() {}
+ESP32ZabbixSender::ESP32ZabbixSender() {}
 
-void ESP8266ZabbixSender::Init(IPAddress ZabbixServerAddr, uint16_t ZabbixServerPort, String ZabbixItemHostName) {
+void ESP32ZabbixSender::Init(IPAddress ZabbixServerAddr, uint16_t ZabbixServerPort, String ZabbixItemHostName) {
 	zAddr = ZabbixServerAddr;
 	zPort = ZabbixServerPort;
 	zItemHost = ZabbixItemHostName;
 }
 
-int ESP8266ZabbixSender::Send(void) { // Send items
+int ESP32ZabbixSender::Send(void) { // Send items
 	int retValue = EXIT_FAILURE;
 	int packetLen = createZabbixPacket();
 	if (zClient.connect(zAddr, zPort)) {
@@ -27,26 +27,29 @@ int ESP8266ZabbixSender::Send(void) { // Send items
 			}
 			delay(10);
 		}
+		
 	}
+	
 	if (retValue != EXIT_SUCCESS) {
 #ifndef SILENT
 		Serial.println("No result");
 #endif
 	}
 	return retValue;
+	zClient.stop();
 }
 
-void ESP8266ZabbixSender::ClearItem(void) { // Clear item list
+void ESP32ZabbixSender::ClearItem(void) { // Clear item list
 	zabbixItemSize = 0;
 }
 
-void ESP8266ZabbixSender::AddItem(String key, float value) {
+void ESP32ZabbixSender::AddItem(String key, float value) {
 	zabbixItemList[zabbixItemSize].key = key;
 	zabbixItemList[zabbixItemSize].val = value;
 	zabbixItemSize++;
 }
 
-int ESP8266ZabbixSender::createZabbixPacket(void) { // [private] create ZabbixPacket
+int ESP32ZabbixSender::createZabbixPacket(void) { // [private] create ZabbixPacket
 	int packetLen = 0;
 	char s[16];
 	String Json = "{\"request\":\"sender data\",\"data\":[";
